b4d11eb087356321f9920eec8db7b92963f625fa
Ani Chidera <chideraani27@gmail.com>
2021-04-09 07:53:45 +0100

    Second commit

--- main.py
+++ main.py
@@ -7,20 +7,20 @@ import matplotlib.pyplot as plt
 import streamlit.components.v1 as components
 
 # ML Libraries
+from sklearn.metrics import accuracy_score
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
 
 
 
-# st.title('Diabetes Detection')
-st.title("Diabetes Detection System")
-# st.markdown("<h1 style='text-align: center; color: pink;'>Diabetes Detection System</h1>", unsafe_allow_html=True)
+st.markdown("<h1 style='text-align: center; color: red;'>DIADETECT</h1>", unsafe_allow_html=True)
+st.markdown("<h4 style='text-align: center; color: white;'>...a diabetes detection system</h4><br>", unsafe_allow_html=True)
 
 
-st.write("This application helps to detect if someone has diabetes using Machine Learning and Python" )
+st.write("Diabetes is a chronic disease that occurs when your blood glucose is too high. This application helps to effectively detect if someone has diabetes using Machine Learning and Python" )
 
-image = Image.open('C:/Users/CHIDERA ANI/Desktop/expert_system/db.jpg')
+image = Image.open('C:/Users/CHIDERA ANI/Desktop/expert_system/db2.jpg')
 st.image(image,use_column_width=True)
 
 #Get the data
@@ -49,19 +49,19 @@ X =  pd.DataFrame(scaler.fit_transform(X), columns=['Pregnancies', 'Glucose', 'B
 # Split the dataset into 75% Training set and 25% Testing set
 x_train, x_test, y_train, y_test = train_test_split(X, y, test_size = 0.3, random_state = 42, stratify=y)
 
-name = st.text_input('Your Name').upper()
+name = st.text_input('What is your name?').upper()
 
 #Get the feature input from the user
 def get_user_input():
 
-    age = st.number_input('Your age')
+    age = st.number_input('Enter your age')
     pregnancies = st.number_input('How many times have you been pregnant?')
-    glucose = st.number_input('Your plasma glucose concentration')
-    blood_pressure = st.number_input('Your blood pressure in mmHg')
-    skin_thickness = st.number_input('Your skin fold thickness in mm')
-    insulin = st.number_input('Your insulin 2-Hour serum in mu U/ml')
-    BMI = st.number_input('Your Body Mass Index')
-    DPF = st.number_input('Your Diabetes Pedigree Function')
+    glucose = st.number_input('What is your plasma glucose concentration?')
+    blood_pressure = st.number_input('What is your blood pressure in mmHg?')
+    skin_thickness = st.number_input('Enter your skin fold thickness in mm')
+    insulin = st.number_input('Enter your insulin 2-Hour serum in mu U/ml')
+    BMI = st.number_input('What is your Body Mass Index?')
+    DPF = st.number_input('What is your Diabetes Pedigree Function?')
       
     
     user_data = {'Age': age,
@@ -84,8 +84,15 @@ if bt:
     rfmodel = RandomForestClassifier(random_state=1)
     rfmodel.fit(x_train, y_train)
     prediction = rfmodel.predict(user_input)
+    # accuracy = accuracy_score(y_true=y_test, y_pred=prediction)
+    # acc = accuracy.format(round(accuracy*100), 2)
+
+
     if prediction == 1:
-        st.write(name," you either have diabetes or are likely to have it. Please visit the doctor as soon as possible.")
+        st.write(name,", you either have diabetes or are likely to have it. Please visit the doctor as soon as possible.")
+        st.write('The accuracy of this prediction is', str(accuracy_score(y_test, rfmodel.predict(x_test)) * 100) + '%' )
+        
     else:
         st.write('Hurray!', name, 'You are diabetes FREE.')
+        st.write('The accuracy of this prediction is',str(accuracy_score(y_test, rfmodel.predict(x_test)) * 100) + '%' )
 
