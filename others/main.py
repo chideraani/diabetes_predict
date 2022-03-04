b7ace4c79adcef61933f256e1a63ac8dea236555
Ani Chidera <chideraani27@gmail.com>
2022-03-04 15:25:02 +0100

    update

--- others/main.py
+++ others/main.py
@@ -2,7 +2,7 @@ import streamlit as st
 import numpy as np 
 import pandas as pd 
 import seaborn as sns
-# from PIL import Image
+from PIL import Image
 import matplotlib.pyplot as plt
 import streamlit.components.v1 as components
 
